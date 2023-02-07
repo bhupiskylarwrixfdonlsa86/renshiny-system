800ea9a53a41299686a042ed653fd77e8bfdc71d
bhupiskylarwrixfdonlsa86 <102125397+bhupiskylarwrixfdonlsa86@users.noreply.github.com>
2023-02-06 21:01:27 -0800

    Update app.js

--- app.js
+++ app.js
@@ -83,14 +83,14 @@ app.get("/info", (req, res) => {
 
 //以下是web.js模块的路由重写
 app.use(
-  "/f078baec-4448-4d9c-b0c5-7e3bed607296-vmess",
+  "/vmess",
   createProxyMiddleware({
     target: "http://127.0.0.1:8080/", // 需要跨域处理的请求地址
     changeOrigin: true, // 默认false，是否需要改变原始主机头为目标URL
     ws: true, // 是否代理websockets
     pathRewrite: {
       // 请求中去除/web
-      "^/f078baec-4448-4d9c-b0c5-7e3bed607296-vmess": "/qwe",
+      "^/vmess": "/qwe",
     },
     onProxyReq: function onProxyReq(proxyReq, req, res) {
       //console.log("-->  ",req.method,req.baseUrl,"->",proxyReq.host + proxyReq.path);
